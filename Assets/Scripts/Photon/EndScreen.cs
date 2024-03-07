cb1df610163c0a67b1663f0e3c75046380a39f84
Jip Verveda <jibril.bear@gmail.com>
2024-03-07 15:25:22 +0100

    End screen stuff

--- Assets/Scripts/Photon/EndScreen.cs
+++ Assets/Scripts/Photon/EndScreen.cs
@@ -1,10 +1,17 @@
 using Photon.Pun;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class EndScreen : MonoBehaviour {
+    [SerializeField] private TMP_Text text;
+
+    private void Start() {
+        text.text = "Your score: " + Points.pointAmount;
+    }
+
     public void Ready() {
         PhotonNetwork.LeaveRoom();
         SceneManager.LoadScene("Lobby");
